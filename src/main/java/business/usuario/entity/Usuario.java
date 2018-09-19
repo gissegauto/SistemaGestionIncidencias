239b3de20208a76503b95608287c75ea9571cade
Gisselle Gauto <giselgaga@gmail.com>
2018-09-19 14:32:52 -0400

    Correccion en entity Clientes

--- src/main/java/business/usuario/entity/Usuario.java
+++ src/main/java/business/usuario/entity/Usuario.java
@@ -6,17 +6,8 @@
 package business.usuario.entity;
 
 import business.funcionario.entity.Funcionario;
-import business.cliente.entity.Cliente;
-import business.configuracion.entity.Notificaciones;
-import business.funcionario.entity.Funcionario;
-import business.funcionario.entity.HistorialFuncionario;
-import business.solicitudes.entity.HistorialSolicitudConexion;
-import business.solicitudes.entity.SolicitudConexion;
 import java.io.Serializable;
-import java.util.Collection;
-import java.util.List;
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -26,12 +17,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
@@ -46,27 +34,6 @@ import javax.xml.bind.annotation.XmlTransient;
     , @NamedQuery(name = "Usuario.findByUsername", query = "SELECT u FROM Usuario u WHERE u.username = :username")
     , @NamedQuery(name = "Usuario.findByPassword", query = "SELECT u FROM Usuario u WHERE u.password = :password")})
 public class Usuario implements Serializable {
-
-    @OneToMany(mappedBy = "idUsuarioActualizacion")
-    private List<Cliente> clienteList;
-    @OneToMany(mappedBy = "idUsuarioRegistro")
-    private List<Cliente> clienteList1;
-
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idUsuarioRegistro")
-    private List<Notificaciones> notificacionesList;
-
-    @OneToMany(mappedBy = "idUsuarioActualizacion")
-    private Collection<HistorialSolicitudConexion> historialSolicitudConexionCollection;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idUsuarioRegistro")
-    private Collection<HistorialSolicitudConexion> historialSolicitudConexionCollection1;
-    @OneToMany(mappedBy = "idUsuarioActualizacion")
-    private Collection<SolicitudConexion> solicitudConexionCollection;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idUsuarioRegistro")
-    private Collection<SolicitudConexion> solicitudConexionCollection1;
-    @OneToMany(mappedBy = "idUsuarioActualizacion")
-    private Collection<Funcionario> funcionarioCollection;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idUsuarioRegistro")
-    private Collection<Funcionario> funcionarioCollection1;
 
     private static final long serialVersionUID = 1L;
     @Id
@@ -86,9 +53,16 @@ public class Usuario implements Serializable {
     @JoinColumn(name = "idFuncionario", referencedColumnName = "idFuncionario")
     @ManyToOne
     private Funcionario idFuncionario;
-
+    
     public Usuario() {
     }
+
+    public Usuario(Integer idusuario, String username, String password) {
+        this.idusuario = idusuario;
+        this.username = username;
+        this.password = password;
+    }
+    
 
     public Usuario(Integer idusuario) {
         this.idusuario = idusuario;
@@ -134,104 +108,10 @@ public class Usuario implements Serializable {
         this.idFuncionario = idFuncionario;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (idusuario != null ? idusuario.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Usuario)) {
-            return false;
-        }
-        Usuario other = (Usuario) object;
-        if ((this.idusuario == null && other.idusuario != null) || (this.idusuario != null && !this.idusuario.equals(other.idusuario))) {
-            return false;
-        }
-        return true;
-    }
 
     @Override
     public String toString() {
-        return "business.usuarios.entity.Usuario[ idusuario=" + idusuario + " ]";
+        return "Usuario{" + "idusuario=" + idusuario + ", username=" + username + ", password=" + password + ", idrol=" + idrol + ", idFuncionario=" + idFuncionario + '}';
     }
-
-    public Collection<HistorialSolicitudConexion> getHistorialSolicitudConexionCollection() {
-        return historialSolicitudConexionCollection;
-    }
-
-    public void setHistorialSolicitudConexionCollection(Collection<HistorialSolicitudConexion> historialSolicitudConexionCollection) {
-        this.historialSolicitudConexionCollection = historialSolicitudConexionCollection;
-    }
-
-    public Collection<HistorialSolicitudConexion> getHistorialSolicitudConexionCollection1() {
-        return historialSolicitudConexionCollection1;
-    }
-
-    public void setHistorialSolicitudConexionCollection1(Collection<HistorialSolicitudConexion> historialSolicitudConexionCollection1) {
-        this.historialSolicitudConexionCollection1 = historialSolicitudConexionCollection1;
-    }
-
-    public Collection<SolicitudConexion> getSolicitudConexionCollection() {
-        return solicitudConexionCollection;
-    }
-
-    public void setSolicitudConexionCollection(Collection<SolicitudConexion> solicitudConexionCollection) {
-        this.solicitudConexionCollection = solicitudConexionCollection;
-    }
-
-    public Collection<SolicitudConexion> getSolicitudConexionCollection1() {
-        return solicitudConexionCollection1;
-    }
-
-    public void setSolicitudConexionCollection1(Collection<SolicitudConexion> solicitudConexionCollection1) {
-        this.solicitudConexionCollection1 = solicitudConexionCollection1;
-    }
-
-    public Collection<Funcionario> getFuncionarioCollection() {
-        return funcionarioCollection;
-    }
-
-    public void setFuncionarioCollection(Collection<Funcionario> funcionarioCollection) {
-        this.funcionarioCollection = funcionarioCollection;
-    }
-
-    public Collection<Funcionario> getFuncionarioCollection1() {
-        return funcionarioCollection1;
-    }
-
-    public void setFuncionarioCollection1(Collection<Funcionario> funcionarioCollection1) {
-        this.funcionarioCollection1 = funcionarioCollection1;
-    }
-
-    @XmlTransient
-    public List<Notificaciones> getNotificacionesList() {
-        return notificacionesList;
-    }
-
-    public void setNotificacionesList(List<Notificaciones> notificacionesList) {
-        this.notificacionesList = notificacionesList;
-    }
-
-    @XmlTransient
-    public List<Cliente> getClienteList() {
-        return clienteList;
-    }
-
-    public void setClienteList(List<Cliente> clienteList) {
-        this.clienteList = clienteList;
-    }
-
-    @XmlTransient
-    public List<Cliente> getClienteList1() {
-        return clienteList1;
-    }
-
-    public void setClienteList1(List<Cliente> clienteList1) {
-        this.clienteList1 = clienteList1;
-    }
-
+    
 }
---
+++ src/main/java/presentation/ChartBean.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package presentation;
+
+import business.cliente.boundary.ClienteManager;
+import business.cliente.entity.Cliente;
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.LineChartModel;
+import org.primefaces.model.chart.LineChartSeries;
+
+/**
+ *
+ * @author ggauto
+ */
+@Named
+@SessionScoped
+public class ChartBean implements Serializable {
+
+    private Cliente cliente;
+
+    @Inject
+    ClienteManager clienteMgr;
+
+    private LineChartModel animatedModel1;
+    private BarChartModel animatedModel2;
+
+    @PostConstruct
+    public void init() {
+        createAnimatedModels();
+    }
+
+    public LineChartModel getAnimatedModel1() {
+        return animatedModel1;
+    }
+
+    public BarChartModel getAnimatedModel2() {
+        return animatedModel2;
+    }
+
+    private void createAnimatedModels() {
+        animatedModel1 = initLinearModel();
+        animatedModel1.setTitle("Line Chart");
+        animatedModel1.setAnimate(true);
+        animatedModel1.setLegendPosition("se");
+        Axis yAxis = animatedModel1.getAxis(AxisType.Y);
+        yAxis.setMin(0);
+        yAxis.setMax(10);
+
+        animatedModel2 = initBarModel();
+        animatedModel2.setTitle("Conexion / Desconexion");
+        animatedModel2.setAnimate(true);
+        animatedModel2.setLegendPosition("ne");
+        yAxis = animatedModel2.getAxis(AxisType.Y);
+        yAxis.setMin(0);
+        yAxis.setMax(200);
+    }
+
+    private BarChartModel initBarModel() {
+        BarChartModel model = new BarChartModel();
+
+        ChartSeries boys = new ChartSeries();
+        boys.setLabel("Conexion");
+        boys.set("2014", 120);
+        boys.set("2015", 100);
+        boys.set("2016", 44);
+        boys.set("2017", 150);
+        boys.set("2018", 25);
+
+        ChartSeries girls = new ChartSeries();
+        girls.setLabel("Desconexion");
+        girls.set("2014", 52);
+        girls.set("2015", 60);
+        girls.set("2016", 110);
+        girls.set("2017", 135);
+        girls.set("2018", 120);
+
+        model.addSeries(boys);
+        model.addSeries(girls);
+
+        return model;
+    }
+
+    private LineChartModel initLinearModel() {
+        LineChartModel model = new LineChartModel();
+
+        LineChartSeries series1 = new LineChartSeries();
+        series1.setLabel("Series 1");
+
+        series1.set(1, 2);
+        series1.set(2, 1);
+        series1.set(3, 3);
+        series1.set(4, 6);
+        series1.set(5, 8);
+
+        LineChartSeries series2 = new LineChartSeries();
+        series2.setLabel("Series 2");
+
+        series2.set(1, 6);
+        series2.set(2, 3);
+        series2.set(3, 2);
+        series2.set(4, 7);
+        series2.set(5, 9);
+
+        model.addSeries(series1);
+        model.addSeries(series2);
+
+        return model;
+    }
+
+}
