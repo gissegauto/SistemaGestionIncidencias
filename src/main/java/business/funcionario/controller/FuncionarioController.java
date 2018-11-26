2e5c95dfed37b22bbd9fcbbbfcf5f6f98b4e363e
ggauto <giselgaga@gmail.com>
2018-11-26 17:44:15 -0300

    Activar - Desactivar Funcionario

--- src/main/java/business/funcionario/controller/FuncionarioController.java
+++ src/main/java/business/funcionario/controller/FuncionarioController.java
@@ -5,14 +5,27 @@
  */
 package business.funcionario.controller;
 
+import business.funcionario.boundary.FuncionarioManager;
+import business.funcionario.entity.Funcionario;
+import business.usuario.entity.Usuario;
+import java.util.Date;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 /**
  *
  * @author ggauto
  */
-
 @Stateless
 public class FuncionarioController {
-    
+
+    @Inject
+    FuncionarioManager funcionarioMgr;
+
+    public void cambiarEstado(Funcionario funcionario, Usuario usuario, String estado) {
+        funcionario.setEstado(estado);
+        funcionario.setFechaActualizacion(new Date());
+        funcionario.setIdUsuarioActualizacion(usuario);
+        funcionario = funcionarioMgr.update(funcionario);
+    }
 }
